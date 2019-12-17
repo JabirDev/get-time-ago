ea143324717c8d23bcdd2a9874cf938af38a3822
Taufan Fatahillah <taufan.fhm8@gmail.com>
2019-12-17 15:44:46 +0700

    add example

--- app/src/main/java/com/jabirdeveloper/gettimeago/MainActivity.java
+++ app/src/main/java/com/jabirdeveloper/gettimeago/MainActivity.java
@@ -2,7 +2,13 @@ package com.jabirdeveloper.gettimeago;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = findViewById(R.id.textView);
+        @SuppressLint("SimpleDateFormat") SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+'");
+        try {
+            Date date = df.parse("2019-12-17T14:33:00+");
+            tv.setText(TimeAgo.get(date));
+        } catch (ParseException e) {
+            e.printStackTrace();
+            tv.setText(e.getMessage());
+        }
 
     }
 }
